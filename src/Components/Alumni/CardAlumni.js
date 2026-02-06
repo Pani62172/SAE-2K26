7880bdef29308dfc62c519ad2459cc77183adbee
ManojVeturi <manojveturi2007@gmail.com>
2026-02-06 19:02:26 +0530

    changes

--- src/Components/Alumni/CardAlumni.js
+++ src/Components/Alumni/CardAlumni.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './cardalumni.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+/*import { faFacebook } from "@fortawesome/free-brands-svg-icons";*/
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Cardalumni = (props) => {
@@ -13,9 +13,9 @@ const Cardalumni = (props) => {
         <div className="bottom-Alumni">
             <div className="items-Alumni">
               <div className="icons-Alumni">
-                <a href={props.facebooklink} target="_blank" rel="noopener noreferrer">
+                {/*<a href={props.facebooklink} target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon className="delay1" icon={faFacebook} color='white' />
-                </a>
+                </a>*/}
                 <a href={props.instalink} target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon className="delay2" icon={faInstagram} color='white'/>
                 </a>
@@ -24,7 +24,7 @@ const Cardalumni = (props) => {
                 </a>
               </div>
             </div>
-            <di className="button-Alumni"><button id='alumni-button'></button></di>
+            <div className="button-Alumni"><button id='alumni-button'></button></div>
         </div>
     </div>
 
