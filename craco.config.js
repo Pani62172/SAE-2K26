822acd1ed9da54501d04c80f907689917d0998d7
atrayeede <atrayeede0025@gmail.com>
2026-03-01 19:31:22 +0530

    fixed bugs

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,30 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
+      // Keep your existing mjs rule (if you had one)
       webpackConfig.module.rules.push({
         test: /\.m?js$/,
         resolve: {
           fullySpecified: false,
         },
       });
+
+      // ←←← THE FIX: modify the existing minimizer (this always works)
+      const cssMinimizer = webpackConfig.optimization.minimizer.find(
+        (plugin) => plugin && plugin.constructor.name === 'CssMinimizerPlugin'
+      );
+
+      if (cssMinimizer) {
+        cssMinimizer.options.minimizerOptions = {
+          preset: [
+            'default',
+            {
+              calc: false,   // stops the "Unexpected '/'" error on Tailwind arbitrary values
+            },
+          ],
+        };
+      }
+
       return webpackConfig;
     },
   },
