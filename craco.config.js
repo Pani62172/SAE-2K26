9f6aae6a131a8cfcb503877b78d33e24fef5c626
atrayeede <atrayeede0025@gmail.com>
2026-03-01 20:43:29 +0530

    Fixed more bugs

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,12 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Keeps your existing rules for modern 3D (.mjs) files
       webpackConfig.module.rules.push({
         test: /\.m?js$/,
         resolve: {
           fullySpecified: false,
         },
       });
-
-      // THE FIX: Completely deletes the CSS Minimizer from the build pipeline
       webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.filter(
         (plugin) => plugin && plugin.constructor.name !== 'CssMinimizerPlugin'
       );
