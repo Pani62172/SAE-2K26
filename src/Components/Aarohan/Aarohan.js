27b858295442c29eeefc7bfa3b4391ccac8a1ba9
atrayeede <atrayeede0025@gmail.com>
2026-03-21 02:22:08 +0530

    fixed card stylings

--- src/Components/Aarohan/Aarohan.js
+++ src/Components/Aarohan/Aarohan.js
@@ -69,17 +69,6 @@ function Aarohan() {
 }, []);
 
 const handleCardMouseMove = (e) => {
-    const card = e.currentTarget;
-    const rect = card.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-    const rotateX = ((y - centerY) / centerY) * -25;
-    const rotateY = ((x - centerX) / centerX) * 25;
-
-    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
   };
 
   const handleCardMouseLeave = (e) => {
@@ -103,8 +92,6 @@ const handleCardMouseMove = (e) => {
       <div key={event.id} className="carousel-item-wrapper w-full flex justify-center">
         <div 
           className="arhn-card"
-          onMouseMove={handleCardMouseMove}
-          onMouseLeave={handleCardMouseLeave}
         >
           <div className="w-full h-full p-4 flex items-center justify-center">
           <img src={imgSrc} alt={event.name} className="w-full h-auto max-h-[600px] object-contain block rounded-lg shadow-lg" />
