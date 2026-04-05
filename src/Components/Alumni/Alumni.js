30f62bd458d2cf74e081faacf4a46f775052cf5e
ManojVeturi <manojveturi2007@gmail.com>
2026-04-05 19:54:20 +0530

    added 2023

--- src/Components/Alumni/Alumni.js
+++ src/Components/Alumni/Alumni.js
@@ -363,6 +363,183 @@ const Alumni = () => {
             "linkedinLink": "https://www.linkedin.com/in/siddharth-prasad-a83336243",
         },
       ]
+    const cardsdata = [
+          {
+              "url":"https://live.staticflickr.com/65535/52768996893_caf48d783b_n.jpg",
+              "name":"Divas Gupta",
+              "instaLink":"https://www.instagram.com/divas__gupta/",
+              "linkedinLink":"https://www.linkedin.com/in/divas-gupta-a23501125/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768499151_e754bb1b90_n.jpg",
+              "name":"Anurag Singh",
+          
+              "instaLink":"https://www.instagram.com/anurag1857/",
+              "linkedinLink":"https://www.linkedin.com/in/anurag1857/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768931815_807e7fd6f3_m.jpg",
+              "name":"Aniket Panigrahi",
+              "instaLink":"https://www.instagram.com/iamaniketpanigrahi16/",
+              "linkedinLink":"https://www.linkedin.com/in/aniketpanigrahi/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52767981077_c0293c8bee_n.jpg" ,
+              "name":"Satyam Shorya Jha",
+              "instaLink":"https://www.instagram.com/ssj11___/",
+              "linkedinLink":"https://www.linkedin.com/in/satyam-shorya-jha-520a19194/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768940625_2bb0d6208a_n.jpg" ,
+              "name":"S. Hemanth",
+              "instaLink":"https://www.instagram.com/hemanthsanagavarapu/",
+              "linkedinLink":"https://www.linkedin.com/in/hemanth-sanagavarapu-7a70161b2/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768790894_9e57425f04_n.jpg" ,
+              "name":"Suryadip Mondal",
+              "instaLink":"https://www.instagram.com/s.u.r.j.o__/",
+              "linkedinLink":"https://www.linkedin.com/in/suryadip-mondal-1534921b1/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768794844_9406bd0899_n.jpg" ,
+              "name":"Sukanya dutta",
+              "instaLink":"https://www.instagram.com/_sukanyadutta_/",
+              "linkedinLink":"https://www.linkedin.com/in/sukanya-dutta-72a45219a/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768004002_1a11c68c93_n.jpg" ,
+              "name":"Soumyadeep Das",
+              "instaLink":"https://instagram.com/sd.soumya_deep?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/soumyadeepdas-20/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769035693_6ac2ab53bd_n.jpg" ,
+              "name":"Anik Mondal",
+              "instaLink":"",
+              "linkedinLink":"",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768572636_b17a8a9e2f_n.jpg" ,
+              "name":"Vishal Jaiswal",
+              "instaLink":"https://www.instagram.com/tynnision_vishal/",
+              "linkedinLink":"https://www.linkedin.com/in/vishal-jaiswal-4b103a199/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768989855_507cd697f9_n.jpg" ,
+              "name":"Souptik Das",
+              "instaLink":"https://instagram.com/soda_o_0_?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/souptik-das-2760ab14a/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768581316_5e948bbcc6_w.jpg" ,
+              "name":"V. Sivaji Ganesh Naidu",
+              "instaLink":"https://instagram.com/sivajiganeshnaidu_v?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/sivaji-ganesh-naidu-vemulapalli-4bb14b1b9/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768838999_c9a8ecaf6f_w.jpg" ,
+              "name":"Ayush Kumar",
+              "instaLink":"https://www.instagram.com/hashtag_aayush/",
+              "linkedinLink":"https://www.linkedin.com/in/aayush-kumar-827976199/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769075718_9f16b7d0b9_w.jpg" ,
+              "name":"Arpan Das",
+              "instaLink":"https://www.instagram.com/the_mystic_shadow/",
+              "linkedinLink":"https://www.linkedin.com/in/arpan-das-217a731b3/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768845264_139784be57_w.jpg" ,
+              "name":"Subham Gupta",
+              "instaLink":"https://www.instagram.com/subhamgupta173/",
+              "linkedinLink":"https://www.linkedin.com/in/subham-gupta-2398361a9/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769081713_12c71e61df_w.jpg" ,
+              "name":"Anshika Chaudhary",
+              "instaLink":"https://www.instagram.com/anshi__kaaaaa/",
+              "linkedinLink":"https://www.linkedin.com/in/anshika-chaudhary-3399561b5/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768596866_b3c5844682_n.jpg" ,
+              "name":"Arani Gangopadhyay",
+              "instaLink":"https://www.instagram.com/arani_008/",
+              "linkedinLink":"https://www.linkedin.com/in/arani-gangopadhyay-7642b6243/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769134248_ab505e843d_n.jpg" ,
+              "name":"Ankita Mondal",
+              "instaLink":"https://www.instagram.com/miss__shady/",
+              "linkedinLink":"https://www.linkedin.com/in/ankita-mandal-b173411ab/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768599596_ca63fa83dd.jpg" ,
+              "name":"Souham Bnaerjee",
+              "instaLink":"https://instagram.com/souhambanerjee?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/souham-banerjee-a407b5185/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/53126268392_96da2b46cf_b.jpg"  ,
+              "name":"Koena Saha"
+      ,        "instaLink":"https://instagram.com/_mistalee_koena?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/koena-saha-869920200/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769130293_712c2ea392_n.jpg" ,
+              "name":"Shivam Kumar",
+              "instaLink":"https://www.instagram.com/k21shivam/",
+              "linkedinLink":"https://www.linkedin.com/in/shivam-kumar21/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769023890_c737f51ed5_w.jpg" ,
+              "name":"Sneha Chattopadhya",
+              "instaLink":"https://www.instagram.com/sneha._.chattopadhyay/",
+              "linkedinLink":"https://www.linkedin.com/in/sneha-chattopadhyay/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769101123_a53708378e_z.jpg" ,
+              "name":"V. Manish Yadav",
+              "instaLink":"/",
+              "linkedinLink":"https://www.linkedin.com/in/manish-veerla/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769028900_6d35be9ed8_n.jpg" ,
+              "name":"Abhishek Sharma",
+              "instaLink":"https://www.instagram.com/_sharma_ab/",
+              "linkedinLink":"https://www.linkedin.com/in/abhishek-sharma-nitdgp23/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769031690_f62cbffc28_n.jpg" ,
+              "name":"Rahul Mondal"
+      ,        "instaLink":"https://instagram.com/r.a.h.u.l_mondal?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/rahul-mondal-86241b200/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768881894_0b1741aa1e_n.jpg" ,
+              "name":"P. Venkatesh"
+      ,        "instaLink":"https://instagram.com/venky_9010?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/perepureddy-venkatesh-ab3420192/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769045905_2ec72118d6_w.jpg" ,
+              "name":"P. Chandra Sekhar",
+              "instaLink":"https://instagram.com/sekhar_691?igshid=YmMyMTA2M2Y=",
+              "linkedinLink":"https://www.linkedin.com/in/chandrasekharnaidu/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52768634016_8ba58bf81e_n.jpg" ,
+              "name":"Leela Prasad",
+              "instaLink":"/",
+              "linkedinLink":"/",
+          },
+          {
+              "url":"https://live.staticflickr.com/65535/52769919127_c24b8a862b_n.jpg" ,
+              "name":"J. sri Chandana",
+              "instaLink":"/",
+              "linkedinLink":"/",
+          },
+      ]
 
       const [activeYear, setActiveYear] = useState("2025");
       
@@ -384,9 +561,10 @@ const Alumni = () => {
         <div class="container-alumni mt-4">
             <h1>Meet Our <span>Alumni</span></h1>
             <div className="content-alumni mt-6">
-                <p id='alumni-content'>The alumni of 2024 and 2025 have played a pivotal role in building the club’s esteemed reputation through their dedicated efforts in organizing a wide range of successful events, including SDV, TEDx, Aarohan, Drag & Drift, F.I.S.T., webinars, and various interactive workshops. Their contributions to the club’s Annual Magazine, Autopia, along with creating content on emerging technologies for Cryptech, have been truly commendable.
-
-In addition, their active participation in national-level competitions such as Baja India has been noteworthy. Overall, their commitment and contributions have been instrumental in the club’s achievements during their tenure, helping to uphold and carry forward its legacy.</p>
+                <p id='alumni-content'>The 2024 and 2025 alumni have significantly contributed to the club's esteemed reputation through their diligent efforts in hosting a range of successful events, including SDV, TEDx, Aarohan, Drag & Drift, F.I.S.T., webinars, and various interactive workshops.
+                Their involvement in the club's Annual Magazine, Autopia, and videos on cutting-edge technology for Cryptech has been remarkable.
+                Furthermore, their participation in national events such as Baja India has been noteworthy.
+                Their contributions have played a crucial role in the club's exceptional achievements during their tenure, thereby ensuring the perpetuation of its legacy.</p>
             </div>
             <div className="year-buttons">
               <button 
@@ -402,8 +580,28 @@ In addition, their active participation in national-level competitions such as B
               >
                 2024
               </button>
+
+              <button 
+                className={activeYear === "2023" ? "active-btn" : ""}
+                onClick={() => setActiveYear("2023")}
+              >
+                2023
+              </button>
             </div>
             <div className="cards-Alumni">
+              {activeYear === "2023" && (
+                <>
+                  {cardsdata.map((card, index) => (
+                    <Cardalumni
+                      key={index}
+                      name={card.name}
+                      image={card.url}
+                      instalink={card.instaLink}
+                      linkedinlink={card.linkedinLink}
+                    />
+                  ))}
+                </>
+              )}
               {activeYear === "2024" && (
                 <>
                   {CardsData.map((card, index) => (
